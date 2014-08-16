685f19d06eee0a1a0e08cd2233846dbfc225cb61
Noon Silk <noonsilk@gmail.com>
2014-08-16 22:38:38 +1000

    import of entire contents of quipper-0.6.tgz.

---
+++ Programs/Tools/Preview.hs
@@ -0,0 +1,27 @@
+-- This file is part of Quipper. Copyright (C) 2011-2014. Please see the
+-- file COPYRIGHT for a list of authors, copyright holders, licensing,
+-- and other details. All rights reserved.
+-- 
+-- ======================================================================
+
+-- ----------------------------------------------------------------------
+-- | This tool reads a circuit from standard input and displays it in
+-- the previewer. For the previewer to work, Acrobat Reader must be
+-- installed. 
+-- 
+-- Note that it is possible to interrupt the circuit generation with
+-- Ctrl-C; in this case, the circuit generated up to that point will
+-- be displayed. A second Ctrl-C will kill the program.
+-- 
+-- Interrupting with Ctrl-C may not work in the Windows operating system.
+
+module Main where
+
+import Quipper
+import QuipperLib.QuipperASCIIParser
+
+-- | Main function: read from 'stdin' and call the previewer. 
+main :: IO ()
+main = do
+  (ins,circuit) <- parse_from_stdin_with_handler
+  print_generic Preview circuit ins
